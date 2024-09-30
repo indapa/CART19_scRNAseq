eed9425b14032e0b697b89b817c8d351db4fbfc4
indapa <indapa@gmail.com>
2024-09-30 06:52:14 +0000

    updated docker ; added nb; adder Rscript

--- RScripts/seuratToh5ad.R
+++ RScripts/seuratToh5ad.R
@@ -1,23 +1,39 @@
-  # Load libraries
-  library(Seurat)
-  library(SeuratDisk)
+# Load libraries
+library(Seurat)
+library(SeuratDisk)
 
-#make function to convert seurat object to h5ad
-# Input: rds file of seurat object, h5ad file to save
-# Output: h5ad file
-
-seuratToh5ad <- function(seuratRDS){
-
-    gz_file <- gzcon(file("seuratRDS", "rb"))
-    rds_object <- readRDS(gz_file)
-    close(gz_file)
-
-    # Update your Seurat object
-    seurat_obj <- UpdateSeuratObject(seurat_obj)
-
-    SaveH5Seurat(seurat_obj, filename = "seurat_object.h5Seurat")
-
-    Convert("seurat_object.h5Seurat", dest = "h5ad")
-
-
+# Function to convert seurat object to h5ad
+# Input: rds file of seurat object
+# Output: h5ad file with name derived from input file
+seuratToh5ad <- function(seuratRDS) {
+  # Extract the file name without extension
+  file_prefix <- tools::file_path_sans_ext(basename(seuratRDS))
+  
+  # Define output file names
+  h5seurat_file <- paste0(file_prefix, ".h5Seurat")
+  h5ad_file <- paste0(file_prefix, ".h5ad")
+  
+  # Check if h5seurat file exists and delete it if it does
+  if (file.exists(h5seurat_file)) {
+    file.remove(h5seurat_file)
+    cat(sprintf("Existing %s file was deleted.\n", h5seurat_file))
+  }
+  
+  # Load the Seurat object
+  seurat_obj <- readRDS(seuratRDS)
+  
+  # Update your Seurat object
+  seurat_obj <- UpdateSeuratObject(seurat_obj)
+  
+  # Save as H5Seurat
+  SaveH5Seurat(seurat_obj, filename = h5seurat_file)
+  
+  # Convert to h5ad
+  Convert(h5seurat_file, dest = "h5ad")
+  
+  cat(sprintf("Conversion to %s completed.\n", h5ad_file))
+  
+  # Optionally, remove the intermediate h5seurat file
+  file.remove(h5seurat_file)
+  cat(sprintf("%s file removed.\n", h5seurat_file))
 }
